fc10158f7837ffa6ac17d431cd5852720150d4c3
Adam <94191657+adamcate@users.noreply.github.com>
2026-02-24 20:25:02 -0700

    gyro test functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,16 @@
 #include <imu.hpp>
 #include <chrono>
 #include <TeensyTimerTool.h>
+#include <algorithm>
 
 using namespace std::chrono_literals;
 using namespace TeensyTimerTool;
-const auto deltaT = 25ms;
+
+const auto deltaT = 5ms;
 // hacky solution for now bc converting from chrono_literals SUCKS :( just make sure they match
-const float deltaTS = 25.f / 1000.f;
+const float deltaTS = 5.f / 1000.f;
+
+#define DRIVE_TIME 3000
 
 Motor myMotors{};
 IMU myIMU{65535, deltaTS};
@@ -21,8 +25,10 @@ IMU myIMU{65535, deltaTS};
 PeriodicTimer t1(RTC); // this uses the real time clock to trigger interrupts, integrating values etc.
 
 void setup() {
-  t1.begin([] {myIMU.Update();}, deltaT); // SHOULD update the IMU data and integrate every 25ms, we'll see lol
+  t1.begin([] {myIMU.Update();}, deltaT); // SHOULD update the IMU data and integrate every 25ms
 }
 
 void loop() {
+  myIMU.printStatus();
+  delay(100);
 }
